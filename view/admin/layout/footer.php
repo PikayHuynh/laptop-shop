2e15bb4ca0eb72092ba5145af7eadec109dc4745
Pikay2305 <HMPhuong20305@gmail.com>
2026-01-14 00:44:32 +0700

    design dashboard admin

---
+++ view/admin/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+<!-- Chart library -->
+<script src="../../resource/admin/template/plugins/chart.min.js"></script>
+<!-- Icons library -->
+<script src="../../resource/admin/template/plugins/feather.min.js"></script>
+<!-- Custom scripts -->
+<script src="../../resource/admin/template/js/script.js"></script>
+</body>
+
+</html>
